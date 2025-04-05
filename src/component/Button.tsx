36838da472f7cfbab781c372579e929e1068f3b5
sreypheak <phorn.sreypheak@institute.pse.ngo>
2025-04-05 10:40:24 +0700

    add navbar and button

---
+++ src/component/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LoginButton() {
+  return (
+    <button className="flex items-center justify-center rounded-md bg-[#30CC37] text-white px-6 py-2.5 font-semibold hover:shadow-lg hover:drop-shadow transition duration-200 hover:bg-[#2F80ED]">
+      Login
+    </button>
+  );
+}
+
+export default LoginButton;
