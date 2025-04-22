3b1d1541860cde49bada978462fb51c852743401
sreypheak <phorn.sreypheak@institute.pse.ngo>
2025-04-22 14:42:08 +0700

    add all

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./component/navbar";
-import Button from "./component/Button";
+import Button from "./component/Button"
 import Login from "./pages/Login";
+import Register from "./pages/register"
 import { Route, Routes } from "react-router";
 function App() {
   const [count, setCount] = useState(0);
@@ -13,6 +14,7 @@ function App() {
         <Route path="/Navbar" element={<Navbar />} />
         <Route path="/Button" element={<Button />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />}/>
       </Routes>
     </>
   );
